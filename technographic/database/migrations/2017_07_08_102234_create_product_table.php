a1446343dbafb92725aaa92c6b71240b8ead34b9
Alvan95 <palvantesha@gmail.com>
2017-07-08 18:31:41 +0700

    techno laravel

---
+++ technographic/database/migrations/2017_07_08_102234_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('nama',50);
+            $table->string('warna',15);
+            $table->string('ukuran',10);
+            $table->string('kategori',10);
+            $table->text('gambar');
+            $table->integer('stok')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ technographic/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+// Route::get('/admin', function () {
+//     return view('template/admintemplate');
+// });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
